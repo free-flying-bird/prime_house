ff4a889d3d557d27c8ca608a2a229b170d8ab458
Sviatoslav Sokolov <sokolik90@yandex.ru>
2021-10-06 12:30:05 +0300

    new version of the website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+// import { set } from "core-js/core/dict";
 import "./style.css";
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
+
+window.onload = () => {
+    document.querySelector('.main__title-first').classList.add('main__title_animation-left');
+    setTimeout(() => {
+        document.querySelector('.main__title-second').classList.add('main__title_animation-right');
+    }, 1500);
+    setTimeout(() => {
+        document.querySelector('.main__title-third').classList.add('main__title_animation-left');
+    }, 3000);
+    setTimeout(() => {
+        document.querySelector('.main__subtitle').classList.add('main__subtitle_animation');
+    }, 5500);
+};
